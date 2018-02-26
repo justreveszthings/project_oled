a54dfbb501cc14c2c0bef14ff73b561a7a467d5b
justreveszthings <justreveszthings@gmail.com>
2018-02-26 21:52:32 +0100

    The first files

---
+++ oled.h
@@ -0,0 +1,10 @@
+/*#############################################################################
+		The header file
+Author:		Tamas Revesz
+E-mail:		justreveszthings@gmail.com
+Date:		2018.02.26.
+About: 		A multifunctional driver for oled displays,
+		that you can use easy with several microcontrollers.
+#############################################################################*/
+
+
